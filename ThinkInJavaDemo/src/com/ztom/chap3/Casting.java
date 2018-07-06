d8aae82fbc5ca12e84fcdede54dbb441c0b5a59f
ZTom000 <ztmooN@qq.com>
2018-07-06 17:53:30 +0800

    20180706006

---
+++ ThinkInJavaDemo/src/com/ztom/chap3/Casting.java
@@ -0,0 +1,14 @@
+package com.ztom.chap3;
+
+public class Casting {
+
+	public static void main(String[] args) {
+		int i = 200;
+		long lng = (long)i;
+		lng = i;
+		long lng2 = (long)200;
+		i = (int) lng2;
+
+	}
+
+}
---
+++ ThinkInJavaDemo/src/com/ztom/chap3/StringOperators.java
@@ -0,0 +1,16 @@
+package com.ztom.chap3;
+
+public class StringOperators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 0, y = 1, z = 2;
+		String s = "x, y, z";
+		System.out.println(s + x + y + z);
+		System.out.println(x + " " + s);
+		s += "(summed) = ";
+		System.out.println(s + (x + y + z));
+		System.out.println("" + x);
+		}
+
+}

fbaa4502f8036ec2e190e3707938992d736592ef
ZTom000 <ztmoon@qq.com>
2018-10-12 18:52:47 +0800

    20181012002

---
+++ ThinkInJavaDemo/src/com/ztom/chap7/parcel10/Parcel11.java
@@ -0,0 +1,27 @@
+package com.ztom.chap7.parcel10;
+
+public class Parcel11 {
+	class Contents{
+		private int i = 11;
+		public int value() {
+			return i;
+		}
+	}
+	
+	class Destination{
+		private String label;
+		public Destination(String whereTo) {
+			label = whereTo;
+		}
+		String readLabel() {
+			return label;
+		}
+	}
+	
+	public static void main(String[] args) {
+		Parcel11 p = new Parcel11();
+		
+		Parcel11.Contents c = p.new Contents();
+		Parcel11.Destination d = p.new Destination("Tanzania");
+	}
+}

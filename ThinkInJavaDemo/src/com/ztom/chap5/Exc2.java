6994d32a62532c324956b66c7103a9813c73bc42
ZTom000 <ztmoon@qq.com>
2018-10-17 11:30:25 +0800

    20181017001

--- ThinkInJavaDemo/src/com/ztom/chap5/Exc2.java
+++ ThinkInJavaDemo/src/com/ztom/chap5/Exc2.java
@@ -2,9 +2,12 @@ package com.ztom.chap5;
 
 class InitStr {
 	String str = "";
+	int[] a;
 	InitStr(String str){
 		this.str = str;
 		System.out.println(this.str);
+		//System.out.println(a);
+		int[] a = {1,2,3};
 	}
 }
 
